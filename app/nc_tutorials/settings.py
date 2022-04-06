48abe290a45b174fd3624decbec8f80ec233b115
Oliver Chang <67518195+ochang89@users.noreply.github.com>
2022-04-05 21:55:57 -0400

    Update settings.py

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
